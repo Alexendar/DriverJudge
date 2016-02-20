71839a0ac110ce69b2c2e831f4c69fea48f45bca
Alexendar <aleksander92@onet.pl>
2016-02-20 01:14:15 +0100

    Initial Commit

---
+++ DriverJudge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DriverJudge
+//
+//  Created by Aleksander on 20/02/16.
+//  Copyright © 2016 Aleksander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DriverJudge/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DriverJudge
+//
+//  Created by Aleksander on 20/02/16.
+//  Copyright © 2016 Aleksander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
